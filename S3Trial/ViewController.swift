88114805c416c643f94eb7c152d9bd524c31f619
Aya <aya_elsahhar@yahoo.com>
2016-03-15 10:55:32 -0400

    Initial Commit

---
+++ S3Trial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S3Trial
+//
+//  Created by Aya on 3/15/16.
+//  Copyright © 2016 Aya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
