7b23e96a5a7f96ff86ed7e1b44eec20cbc261f9c
Aya <aya_elsahhar@yahoo.com>
2016-03-29 11:37:03 -0400

    createing constant file

---
+++ S3Trial/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  S3Trial
+//
+//  Created by Aya on 3/29/16.
+//  Copyright © 2016 Aya. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import AWSS3
+
+let COGNITO_REGION = AWSRegionType.USEast1 // select your region
+let SERVICE_REGION = AWSRegionType.USEast1 // select your region
+let COGNITO_POOL_ID = "Your_Identity_Pool_Id"
+let S3_BUCKET_NAME = "Your_Bucket_name"
